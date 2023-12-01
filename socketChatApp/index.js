0192dd13861fb4ffdef8d9c7c49291addbd765e1
Lily Barrett <11barrettpl@gmail.com>
2023-12-02 11:50:17 +1300

    Done!!

--- socketChatApp/index.js
+++ socketChatApp/index.js
@@ -5,16 +5,23 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+const connectedUsers = new Set();
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 //Q1 - broadcast a message to all users when a user connects 
 io.on('connection', (socket) => {
+    connectedUsers.add(socket.id);
+
+    io.emit('update online users', Array.from(connectedUsers)); //NOT SURE ABT THIS HERE
     io.emit('chat message', 'A user has connected');
 
     // Q2 - add support for adding nicknames
     socket.on('set nickname', (nickname) => {
         socket.nickname = nickname;
+        connectedUsers.add(socket.nickname);
+        io.emit('update online users', Array.from(connectedUsers));
             socket.broadcast.emit('chat message', `${nickname} has joined the chat`);
 
     });
@@ -32,6 +39,8 @@ io.on('connection', (socket) => {
     });
         //Q1 - broadcast a message when a user disconnects
     socket.on('disconnect', () => {
+        connectedUsers.delete(socket.nickname);
+        io.emit('update online users', Array.from(connectedUsers));
             socket.broadcast.emit('chat message', `${socket.nickname} has left the chat`);
         });
     });
